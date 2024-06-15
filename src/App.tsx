128cea4d3f6d94ce2d7c86da5d4f175a8971c17a
vojb <vojbruhn@gmail.com>
2024-06-15 11:49:40 +0200

    add upload photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
-import React, { useRef, useState } from "react";
+import React, { ChangeEventHandler, useRef, useState } from "react";
 import "./App.scss";
 import ShowCard, { Card } from "./components/show-card/show-card";
 import { TextField } from "@mui/material";
 import { toPng } from "html-to-image";
-
+export const fileToDataString = (file: File) => {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onerror = (error) => reject(error);
+    reader.onload = () => resolve(reader.result as string);
+  });
+};
 function App() {
   const elementRef = useRef<HTMLDivElement>(null); // Correct type for the ref
   const [card, setCard] = useState<Card | null>(null); // Initial state as null
   const [name, setName] = useState("");
+  const [selectedImage, setSelectedImage] = useState<File>();
+  const [previewImgUrl, setPreviewimgUrl] = useState("");
 
+  const handleFileChange: ChangeEventHandler<HTMLInputElement> = async (
+    event
+  ) => {
+    const file = event.target.files as FileList;
+    setSelectedImage(file?.[0]);
+    if (!file) {
+      return;
+    }
+    try {
+      const imgUrl = await fileToDataString(file?.[0]);
+      setPreviewimgUrl(imgUrl);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const htmlToImageConvert = () => {
     if (elementRef.current) {
       toPng(elementRef.current, { cacheBust: false })
@@ -28,7 +52,7 @@ function App() {
     <div style={{ display: "flex" }} className="App">
       <div>
         <div ref={elementRef}>
-          <ShowCard card={{ name: name }} />
+          <ShowCard card={{ name: name, image: previewImgUrl }} />
         </div>
         <button onClick={htmlToImageConvert}>Download Image</button>
         <TextField
@@ -36,6 +60,11 @@ function App() {
           value={name}
           onChange={(e) => setName(e.target.value.toUpperCase())}
         />
+        <div className="wrapper">
+          <form>
+            <input type="file" onChange={handleFileChange} accept="image/*" />
+          </form>
+        </div>
       </div>
     </div>
   );
