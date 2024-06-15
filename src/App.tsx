b850c9c3a23f4b59a80a5ae7f429c9bb9c07e79c
vojb <vojbruhn@gmail.com>
2024-06-15 11:26:43 +0200

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import "./App.scss";
 import ShowCard, { Card } from "./components/show-card/show-card";
 import { TextField } from "@mui/material";
 import { toPng } from "html-to-image";
+
 function App() {
-  const elementRef = useRef<HTMLElement>();
-  const [card, setCard] = useState<Card>();
+  const elementRef = useRef<HTMLDivElement>(null); // Correct type for the ref
+  const [card, setCard] = useState<Card | null>(null); // Initial state as null
   const [name, setName] = useState("");
+
   const htmlToImageConvert = () => {
-    if (elementRef != null) {
+    if (elementRef.current) {
       toPng(elementRef.current, { cacheBust: false })
         .then((dataUrl) => {
           const link = document.createElement("a");
@@ -21,11 +23,12 @@ function App() {
         });
     }
   };
+
   return (
     <div style={{ display: "flex" }} className="App">
-      <div style={{}}>
+      <div>
         <div ref={elementRef}>
-          <ShowCard card={{ name: name }}></ShowCard>
+          <ShowCard card={{ name: name }} />
         </div>
         <button onClick={htmlToImageConvert}>Download Image</button>
         <TextField
