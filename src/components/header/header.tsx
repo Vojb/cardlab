eabcf1d27c7268cd68bdfc506ec442de7420dfe5
vojb <vojbruhn@gmail.com>
2024-07-29 09:17:13 +0200

    fix background size

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,17 +1,32 @@
 import { KortLabbetLogo } from "../../assets/svg/kortlabbet-logo";
 import styles from "./header.module.scss";
-import Menu from "../meny/menu";
+import Menu, { menuItems } from "../meny/menu";
+import { Link, ListItemText } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <nav className={styles.headerRoot}>
       <div className={styles.logoWrapper}>
-        <div className={styles.logo}>
+        <div className={styles.logo} onClick={() => navigate("/")}>
           <KortLabbetLogo></KortLabbetLogo>
         </div>
       </div>
 
-      <div>
+      <div className={styles.drawerMobile}>
         <Menu />
+      </div>
+      <div className={styles.menu}>
+        {menuItems.map((item) => {
+          return (
+            <ListItemText
+              style={{ cursor: "pointer" }}
+              secondary={item.text}
+              onClick={() => navigate(item.navigate)}
+            />
+          );
+        })}
       </div>
     </nav>
   );
